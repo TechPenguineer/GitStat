d2f0ce05f582854d00ece35bf5e0f0b4ee4d4e33
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-03 13:57:19 -0400

    create name space

--- src/getApi.cs
+++ src/getApi.cs
@@ -1,5 +1,8 @@
-public static class getApi{
-    public void retrieveAPI(string apiType, string ownerName, string repositoryName ){
-        
+namespace apiRetriver
+{
+    public static class getApi{
+        public void retrieveAPI(string apiType, string ownerName, string repositoryName ){
+              public enum apiTypes = {followers, following,stared}
+            }
     }
 }
